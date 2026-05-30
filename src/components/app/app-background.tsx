28192f94fa8f56df61adb73a16663b30ba9cced7
Matheus Augusto Fraga Correa <matheusguto1@hotmail.com>
2026-05-29 23:21:18 -0300

    refac: Removendo glow para que melhore desempenho do site no mobile

--- src/components/app/app-background.tsx
+++ src/components/app/app-background.tsx
@@ -2,9 +2,6 @@ import { DotPattern } from "../ui/dot-pattern";
 
 export default function AppBackground() {
   return (
-    <DotPattern
-      className="-z-50 opacity-20 fixed inset-0 pointer-events-none"
-      glow
-    />
+    <DotPattern className="-z-50 opacity-20 fixed inset-0 pointer-events-none" />
   );
 }
